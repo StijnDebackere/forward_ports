d96bf8d4da5ff955032e87015ce35971b6531c97
Stijn <debackere@strw.leidenuniv.nl>
2021-07-01 10:13:33 +0200

    Allow list of ports.

--- forward_ports.py
+++ forward_ports.py
@@ -6,18 +6,19 @@ import subprocess
 parser = argparse.ArgumentParser(description='Process some integers.')
 
 parser.add_argument(
-    "port", default=8888, type=int, help="Port to forward."
+    "host", default="purmer", type=str, help="Host to forward."
 )
 parser.add_argument(
-    "host", default="purmer", type=str, help="Host to forward."
+    "-p", "--ports", default=8888, nargs="+", type=int, help="Port(s) to forward."
 )
 parser.add_argument(
     "-r", "--remote", type=str, help="Also forward port on host to remote."
 )
 
 
-def forward_port_to_remote(port, host, remote):
-    subprocess.run(["echo", f"Forwarding port {port} to host {host} and remote {remote}"])
+def forward_port_to_remote(ports, host, remote):
+    for port in ports:
+        subprocess.run(["echo", f"Forwarding port {port} to host {host} and remote {remote}"])
     if remote is not None:
         proc = subprocess.run(["ssh", "-f", host, f"ssh -NfL localhost:{port}:localhost:{port} {remote}"])
 
@@ -26,4 +27,4 @@ def forward_port_to_remote(port, host, remote):
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    forward_port_to_remote(port=args.port, host=args.host, remote=vars(args).get("remote", None))
+    forward_port_to_remote(ports=args.ports, host=args.host, remote=vars(args).get("remote", None))
